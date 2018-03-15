f71a1f5545de12056810ebfc3124ce4616fb9276
root <root@sndcapcf0050.americas.svc.corpintra.net>
2018-03-15 09:40:54 +0100

    ssl validity inital commit

--- check_SSL_validity.sh
+++ check_SSL_validity.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-certificate_expiry=`( export DOMAIN=test.meapp-sys-an-int-ndc1.americas.bg.corpintra.net:443;echo $(( ( $(date +%s -d "$( echo | openssl s_client -servername $DOMAIN -connect $DOMAIN 2>/dev/null | openssl x509 -noout -enddate | sed 's/.*notAfter=\(.*\)/\1/g' )" ) - $(date +%s) ) / 86400 )) )`
+#certificate_expiry=`( export DOMAIN=test.meapp-sys-an-int-ndc1.americas.bg.corpintra.net:443;echo $(( ( $(date +%s -d "$( echo | openssl s_client -servername $DOMAIN -connect $DOMAIN 2>/dev/null | openssl x509 -noout -enddate | sed 's/.*notAfter=\(.*\)/\1/g' )" ) - $(date +%s) ) / 86400 )) )`
 
-echo $certificate_expiry
+#echo $certificate_expiry
 
-exit 0
+#exit 0
 
+echo "Hello World"
